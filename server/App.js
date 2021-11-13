48b9c3f4557c3c555c08bd73613715ef493a8569
Nikita <nik44226269@yandex.ru>
2021-11-13 14:35:55 +0300

    initial

---
+++ server/App.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3001;
+const blogRouter = require('../src/routes/blog.router')
+
+app.use(express.json());
+app.use(cors({ credentials: true, origin: true }));
+
+app.use('/blog', blogRouter)
+
+app.listen(PORT, () => {
+  console.log('Server is connected on port: ', PORT)
+});
+
